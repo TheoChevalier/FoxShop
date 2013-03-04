8411330f33a739aa7821ac8aff151f57012fc0f7
TheoChevalier <theo.chevalier11@gmail.com>
2013-03-04 06:52:33 +0100

    Finish email

--- php/email.php
+++ php/email.php
@@ -5,19 +5,42 @@ $tab = json_decode($_POST["data"], true);
 $items = json_decode($tab["items"], true);
 print_r($tab);
 print_r($items);
+
 if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $tab['email']))
   $passage_ligne = "\r\n";
 else
   $passage_ligne = "\n";
-$subject = "Liste de courses « ".$tab["name"]." » envoyée depuis l’application Shopping List".$passage_ligne;
+
+foreach ($tab as $key => $value) {
+  if($key =="name") {
+    $list.= $passage_ligne."<p>Liste de courses « ".$value." » envoyée depuis <a href='http://theochevalier.github.com/FoxShop'>l’application FoxShop</a>.</p>";
+  }
+  if($key =="items") {
+    foreach ($value as $keyItems => $items) {
+      foreach ($items as $keyItem => $item) {
+        if($keyItem =="name") {
+          $list .= $passage_ligne."<br>- ".$item;
+        }
+        if ($keyItem == "nb" && $item > 1) {
+          $list .= $passage_ligne." x".$item;
+        }
+        if ($keyItem == "done" && $item == "1") {
+          $list .= $passage_ligne." (Acheté &#10003;)";
+        }
+      }
+    }
+  }
+}
+
+$subject = "Liste de courses « ".$tab["name"]." »".$passage_ligne;
 $mail_notif = '<html>
 <head>
 <title>Liste de courses « '.$tab["name"].' »</title>
 </head>
-<body>'.$tab["items"].$items["name"].'</body>
+<body>'.$list.'</body>
 </html>';
 $headers  = 'MIME-Version: 1.0' . $passage_ligne;
 $headers .= 'Content-type: text/html; charset=utf-8' . $passage_ligne;
-$headers .= 'From: "Shopping List" <no-reply@theochevalier.fr>' . $passage_ligne;
+$headers .= 'From: "FoxShop" <no-reply@theochevalier.fr>' . $passage_ligne;
 mail($_POST["email"], '=?UTF-8?B?'.base64_encode($subject).'?=', $mail_notif, $headers);
 ?>
