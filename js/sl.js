0c1ef89fb2310186fe2bb860ef9d5e922cb8ac26
TheoChevalier <theo.chevalier11@gmail.com>
2013-03-23 16:58:42 +0100

    localize update prompt

--- js/sl.js
+++ js/sl.js
@@ -33,7 +33,7 @@
   var appCache = window.applicationCache;
     if (appCache) {
       appCache.onupdateready = function () {
-        if (confirm("The app has been updated. Do you want to download the latest files? \nOtherwise they will be updated at the next reload.")) {
+        if (confirm(_("update-ready"))) {
           location.reload(true);
         }
       };
